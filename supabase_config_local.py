e61b6c6afabf70db570b010654657984c394033c
janacibron <acibronjan@gmail>
2026-08-16 10:43:58 +0800

    Remove secret config from git tracking

--- supabase_config_local.py
+++
@@ -1,8 +0,0 @@
-import os
-
-SUPABASE_URL = os.environ.get("SUPABASE_URL", "https://xmpakpmhzioxwaiwijnn.supabase.co")
-SUPABASE_PUBLISHABLE_KEY = os.environ.get("SUPABASE_PUBLISHABLE_KEY", "")
-SUPABASE_SECRET_KEY = os.environ.get("SUPABASE_SECRET_KEY", "")
-
-if not SUPABASE_SECRET_KEY:
-    SUPABASE_SECRET_KEY = os.environ.get("SUPABASE_SERVICE_ROLE_KEY", "")
